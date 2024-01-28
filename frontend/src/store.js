8bc93c7c2dadea104b6857e1cf5c73a671830acc
Nupun Sawant <nupunsawant3930@gmail.com>
2024-01-28 18:46:41 +0530

    Step 21 and 22 : Introduction to redux and created reducer files

---
+++ frontend/src/store.js
@@ -0,0 +1,21 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { thunk } from "redux-thunk";
+
+import { productListReducer } from "./reducers/productReducer";
+
+const reducer = combineReducers({
+    productList: productListReducer,
+});
+
+const initialState = {};
+
+const middlewares = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+export default store;
