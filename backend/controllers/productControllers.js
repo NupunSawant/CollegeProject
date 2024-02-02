7b16acaacfc125927d76d38f4f0305122073f7c0
Nupun Sawant <nupunsawant3930@gmail.com>
2024-02-02 22:40:09 +0530

    Step 30: Clean up tha backend route by extracting their logic into controllers

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,35 @@
+import asyncHandler from "express-async-handler";
+
+import Product from "../models/productModel.js";
+
+/**
+ * @desc		Get all products
+ * @route		GET /api/products
+ * @access	public
+ */
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+/**
+ * @desc		Get single product
+ * @route		GET /api/products/:id
+ * @access	public
+ */
+const getProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not Found" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.json(err);
+  }
+});
+
+export { getProducts, getProductById };
