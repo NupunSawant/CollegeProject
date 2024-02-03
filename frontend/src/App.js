7db8694f1d557312b236bbcfb13cd290c3404327
Nupun Sawant <nupunsawant3930@gmail.com>
2024-02-03 20:18:24 +0530

    Step 39: User Register, Constants, Reducer, Action and Screen completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,29 +7,31 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Flex
-        as="main"
-        mt="72px"
-        direction="column"
-        py="6"
-        px="6"
-        bgColor="gray.200">
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/products/:id" element={<ProductScreen />} />
-          <Route path="/cart/" element={<CartScreen />} />
-          <Route path="/cart/:id" element={<CartScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-        </Routes>
-      </Flex>
-      <Footer />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Flex
+				as="main"
+				mt="72px"
+				direction="column"
+				py="6"
+				px="6"
+				bgColor="gray.200">
+				<Routes>
+					<Route path="/" element={<HomeScreen />} />
+					<Route path="/products/:id" element={<ProductScreen />} />
+					<Route path="/cart/" element={<CartScreen />} />
+					<Route path="/cart/:id" element={<CartScreen />} />
+					<Route path="/login" element={<LoginScreen />} />
+					<Route path="/register" element={<RegisterScreen />} />
+				</Routes>
+			</Flex>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
