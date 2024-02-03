d629951ba532e9e32d53049bde818a8dc8be4209
Nupun Sawant <nupunsawant3930@gmail.com>
2024-02-03 19:18:12 +0530

    Step 38: Header modification to show user and user logout and error in loginScreen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,56 +1,99 @@
-import { Box, Flex, Heading, Icon, Link } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Flex,
+	Heading,
+	Icon,
+	Link,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import { HiOutlineMenuAlt3, HiShoppingBag, HiUser } from "react-icons/hi";
-import { Link as RouterLink } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 
+import { IoChevronDown } from "react-icons/io5";
+import { logout } from "../actions/userActions";
 import HeaderMenuItem from "./HeaderMenuItem";
 
 const Header = () => {
-  const [show, setShow] = useState(false);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  return (
-    <Flex
-      as="header"
-      alignItems="center"
-      justifyContent="space-between"
-      wrap="wrap"
-      zIndex='99999'
-      py="6"
-      px="6"
-      bgColor="gray.800"
-      w="100%"
-      pos="fixed"
-      top="0"
-      left="0">
-      {/* Title/Logo */}
-      <Link as={RouterLink} to="/">
-        <Heading
-          as="h1"
-          color="whiteAlpha.800"
-          fontWeight="bold"
-          size="md"
-          letterSpacing="wide">
-          My Store
-        </Heading>
-      </Link>
+	const [show, setShow] = useState(false);
 
-      {/* Hamburger Menu */}
-      <Box
-        display={{ base: "block", md: "none" }}
-        onClick={() => setShow(!show)}>
-        <Icon as={HiOutlineMenuAlt3} color="white" w="6" h="6" />
-      </Box>
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
 
-      {/* Menu */}
-      <Box
-        dispay={{ base: show ? "block" : "none", md: "flex" }}
-        width={{ base: "full", md: "auto" }}
-        mt={{ base: 4, md: 0 }}>
-        <HeaderMenuItem icon={HiShoppingBag} label="cart" url="/cart" />
-        <HeaderMenuItem icon={HiUser} label="Login" url="/login" />
-      </Box>
-    </Flex>
-  );
+	const logoutHandler = () => {
+		dispatch(logout());
+		navigate("/login");
+	};
+
+	return (
+		<Flex
+			as="header"
+			alignItems="center"
+			justifyContent="space-between"
+			wrap="wrap"
+			zIndex="99999"
+			py="6"
+			px="6"
+			bgColor="gray.800"
+			w="100%"
+			pos="fixed"
+			top="0"
+			left="0">
+			{/* Title/Logo */}
+			<Link as={RouterLink} to="/">
+				<Heading
+					as="h1"
+					color="whiteAlpha.800"
+					fontWeight="bold"
+					size="md"
+					letterSpacing="wide">
+					My Store
+				</Heading>
+			</Link>
+
+			{/* Hamburger Menu */}
+			<Box
+				display={{ base: "block", md: "none" }}
+				onClick={() => setShow(!show)}>
+				<Icon as={HiOutlineMenuAlt3} color="white" w="6" h="6" />
+			</Box>
+
+			{/* Menu */}
+			<Box
+				dispay={{ base: show ? "block" : "none", md: "flex" }}
+				width={{ base: "full", md: "auto" }}
+				mt={{ base: 4, md: 0 }}>
+				<HeaderMenuItem icon={HiShoppingBag} label="cart" url="/cart" />
+
+				{userInfo ? (
+					<Menu>
+						<MenuButton
+							as={Button}
+							rightIcon={<IoChevronDown />}
+							_hover={{ textDecor: "none", opacity: "0.7" }}>
+							{userInfo.name}
+						</MenuButton>
+						<MenuList>
+							<MenuItem as={RouterLink} to="/profile">
+								Profile
+							</MenuItem>
+							<MenuItem onClick={logoutHandler}>Logout</MenuItem>
+						</MenuList>
+					</Menu>
+				) : (
+					<HeaderMenuItem icon={HiUser} label="login" url="/login" />
+				)}
+			</Box>
+		</Flex>
+	);
 };
 
 export default Header;
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,20 +1,20 @@
 import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Link,
-  Spacer,
-  Text,
+	Button,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Input,
+	Link,
+	Spacer,
+	Text,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from " react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  Link as RouterLink,
-  useNavigate,
-  useSearchParams,
+	Link as RouterLink,
+	useNavigate,
+	useSearchParams,
 } from "react-router-dom";
 
 import { login } from "../actions/userActions";
@@ -22,79 +22,79 @@ import FormContainer from "../components/FormContainer";
 import Message from "../components/Message";
 
 const LoginScreen = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const [searchParams] = useSearchParams();
-  let redirect = searchParams.get("redirect") || "/";
+	const [searchParams] = useSearchParams();
+	let redirect = searchParams.get("redirect") || "/";
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo, loading, error } = userLogin;
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo, loading, error } = userLogin;
 
-  useEffect(() => {
-    if (userInfo) {
-      navigate(redirect);
-    }
-  }, [navigate, userInfo, redirect]);
+	useEffect(() => {
+		if (userInfo) {
+			navigate(redirect);
+		}
+	}, [navigate, userInfo, redirect]);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    dispatch(login(email, password));
-  };
+	const submitHandler = (e) => {
+		e.preventDefault();
+		dispatch(login(email, password));
+	};
 
-  return (
-    <Flex w="full" alignItems="center" justifyContent="center" py="5">
-      <FormContainer>
-        <Heading as="h1" mb="8" fontSize="3xl">
-          Login
-        </Heading>
+	return (
+		<Flex w="full" alignItems="center" justifyContent="center" py="5">
+			<FormContainer>
+				<Heading as="h1" mb="8" fontSize="3xl">
+					Login
+				</Heading>
 
-        {error && <Message type="error">{error}</Message>}
+				{error && <Message type="error">{error}</Message>}
 
-        <form onSubmit={submitHandler}>
-          <FormControl id="email">
-            <FormLabel htmlfor="email"> Email Address</FormLabel>
-            <Input
-              id="email"
-              type="email"
-              placeholder="username@domain.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
+				<form onSubmit={submitHandler}>
+					<FormControl id="email">
+						<FormLabel htmlfor="email"> Email Address</FormLabel>
+						<Input
+							id="email"
+							type="email"
+							placeholder="username@domain.com"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</FormControl>
 
-          <Spacer h="3" />
+					<Spacer h="3" />
 
-          <FormControl id="password">
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              id="password"
-              type="password"
-              placeholder="************"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
+					<FormControl id="password">
+						<FormLabel htmlFor="password">Password</FormLabel>
+						<Input
+							id="password"
+							type="password"
+							placeholder="************"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</FormControl>
 
-          <Button type="submit" colorScheme="teal" mt="4" isLoading={loading}>
-            Login
-          </Button>
-        </form>
+					<Button type="submit" colorScheme="teal" mt="4" isLoading={loading}>
+						Login
+					</Button>
+				</form>
 
-        <Flex pt="10">
-          <Text fontWeight="semibold">
-            New Customer?{""}
-            <Link as={RouterLink} to="/register">
-              Click here to Register
-            </Link>
-          </Text>
-        </Flex>
-      </FormContainer>
-    </Flex>
-  );
+				<Flex pt="10">
+					<Text fontWeight="semibold">
+						New Customer?{""}
+						<Link as={RouterLink} to="/register">
+							Click here to Register
+						</Link>
+					</Text>
+				</Flex>
+			</FormContainer>
+		</Flex>
+	);
 };
 
 export default LoginScreen;
